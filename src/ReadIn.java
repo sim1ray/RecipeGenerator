5d14d2583fb121f20e119ba0fe4bdacdb100f00c
sellc <chris.d.sellers@gmail.com>
2019-04-13 15:59:16 -0700

    ReadIn class for .csv
    ReadIn class

---
+++ src/ReadIn.java
@@ -0,0 +1,44 @@
+
+public class ReadIn {
+  public Day[] readInDays(String filePath) {
+    File excelFile = new File(filePath);
+    int totalTradingDays = 255;
+    Day[] days = new Day[totalTradingDays];
+    Scanner reader;
+    try {
+      reader = new Scanner(excelFile);
+      String input = reader.nextLine();
+      Scanner lineInput = null;
+      int index = 0;
+      while (reader.hasNextLine()) {
+        input = reader.nextLine();
+        if (!input.contains("This document was made with Webix library. http://webix.com")) {
+          lineInput = new Scanner(input);
+          lineInput.useDelimiter(",");
+          days[index] = new Day(lineInput.next(), lineInput.nextDouble(), lineInput.nextDouble(),
+              lineInput.nextDouble(), lineInput.nextDouble(), lineInput.nextInt());
+            index++;
+          lineInput.close();
+        }
+      }
+      reader.close();
+      if (index < totalTradingDays) {
+        days = trimArray(days, index);
+      }
+      return days;
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private Day[] trimArray(Day[] array, int length) {
+    Day[] newArray = new Day[length];
+    int index = 0;
+    while (index < length) {
+      newArray[index] = array[index];
+      index++;
+    }
+    return newArray;
+  }
+}
